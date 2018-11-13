f3915d12d89f78f942e9e575c61d75a71637826d
Yaroslavik <yaroslav4167@mail.ru>
2018-11-13 23:45:02 +0200

    Update optional mods.. Again...
    Поддержка многослойного дерева вложенности / новая механика выбора одного из национальных модов.
    Сортировка дерева производится так же, как и в прошлой версии (Сначала всё как config.js, потом всё остальное). Необходимо соблюдать порядок, это важно. Т.к. дерево на нём завязано. То есть модификации должны быть по порядку {Кор-мод}{Мод}{Аддон}, с соответствующими им уровнями вложенности.
    
    Теперь выбор "одного из" работает со всеми модами/субмодами. (К примеру: При включении миникарты - отключится неи со всеми его аддонами. Т.к. NEI и миникарта имели общий флаг группы [onlyOneGroup]  и соответственно включенный параметр [onlyOne])
    
    Видео с демонстрацией работы вложенности/зависимости и функции выбора "одного из": https://lww.page.link/FKec

--- Launcher/runtime/config.js
+++ Launcher/runtime/config.js
@@ -1,13 +1,15 @@
 // ====== LAUNCHER CONFIG ====== //
 var config = {
-    dir: "gravitlauncher", // Launcher directory
-    title: "Gravit Minecraft Launcher", // Window title
+    dir: "GravitLauncher", // Launcher directory
+    title: "GravitLauncher", // Window title
     icons: [ "favicon.png" ], // Window icon paths
 
     // Auth config
-    newsURL: "https://github.com/GravitLauncher/Launcher/releases", // News WebView URL
-    linkText: "GravitLauncher GitHub", // Text for link under "Auth" button
-    linkURL: new java.net.URL("https://github.com/GravitLauncher/Launcher"), // URL for link under "Auth" button
+    linkText: "GravitLauncher", // Text for link under "Auth" button
+    linkURL: new java.net.URL("https://gravitlauncher.ml"), // URL for link under "Auth" button
+	
+    // Menu config
+    discord_url: new java.net.URL("https://discord.gg/bf7ZtwC"),
 
     // Settings defaults
     settingsMagic: 0xC0DE5, // Ancient magic, don't touch
@@ -17,11 +19,39 @@ var config = {
 };
 
 // ====== DON'T TOUCH! ====== //
-var dir = IOHelper.HOME_DIR.resolve(config.dir);
-if (!IOHelper.isDir(dir)) {
-    java.nio.file.Files.createDirectory(dir);
+
+DirBridge.dir = IOHelper.HOME_DIR.resolve(config.dir);
+if (!IOHelper.isDir(DirBridge.dir)) {
+    java.nio.file.Files.createDirectory(DirBridge.dir);
 }
-var defaultUpdatesDir = dir.resolve("updates");
-if (!IOHelper.isDir(defaultUpdatesDir)) {
-    java.nio.file.Files.createDirectory(defaultUpdatesDir);
+DirBridge.defaultUpdatesDir = DirBridge.dir.resolve("updates");
+if (!IOHelper.isDir(DirBridge.defaultUpdatesDir)) {
+    java.nio.file.Files.createDirectory(DirBridge.defaultUpdatesDir);
+}
+
+//====== SERVERS CONFIG ====== //
+var serversConfig = {
+    defaults: {
+        // Лозунг сервера
+        description: "Мир в котором возможно все"
+    },
+     getServerProperty: function(profile, property){
+        if(serversConfig[profile]==null || serversConfig[profile][property]==null){
+          return serversConfig.defaults[property];
+        }
+        return serversConfig[profile][property];
+    }
+};
+
+var optModNames = {
+   optAutoModName: true,//Попытатся автоматически создать представляемое имя модификации
+   modInfo: {//"Путь до опц. модификации" : "Отображаемый клиенту контент"
+     /*"mods/ModName-1.1.jar": {
+        name: "ModName", //Наименование модификации (Отображаемое в лаунчере) [Можно не указывать]
+        description:"Лучший в своём роде ModName.", //Описание модификации [Можно не указывать]
+        subTreeLevel: 1 //Это суб-модификация? Уровень вложенности модификации (Ядро - 1, Мод - 2, Аддон - 3 и т.д...) (будет произведён отступ от левого края для выделения) [Можно не указывать, по умолчанию: 1]
+        onlyOne: true, //Для выбора только одной из группы модификаций [Можно не указывать]
+        onlyOneGroup: 1 //Используется в совокупности с onlyOne. Определяет ту самую группу, из которой производится выбор.
+     },*/
+   },
 }
